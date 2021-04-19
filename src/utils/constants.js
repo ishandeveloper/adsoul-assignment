5c3c3c2499dddd8533b76cb0d77d4519da583dcb
Ishan Sharma <contact@ishandeveloper.com>
2021-04-19 15:51:18 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-export const APP_LIST_API = "https://api.npoint.io/54d09df281f91e8c146f";
+export const APP_LIST_API = "https://api.npoint.io/4ca5aaf459a573940672";
 export const ALL_STATS_API = "https://api.npoint.io/d734975d2aee62d197ef";
 export const SINGLE_STAT_API = "https://api.npoint.io/d734975d2aee62d197ef/";
